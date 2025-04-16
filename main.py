70db4b57ed31ca9e232c54460297e4bb13411b76
sky17-10 <sky1710@g.skku.edu>
2025-04-16 19:34:02 +0900

    Add main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def two_sum(nums: List[int], target: int) -> List[int]:
         for j in range(i + 1, n):
             if nums[i] + nums[j] == target:
                 return [i, j]
-    return []
+    return 
 
 
 print(two_sum([2, 7, 11, 15], 9))
